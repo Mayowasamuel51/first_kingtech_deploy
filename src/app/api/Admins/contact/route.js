095911699aad69656f3399c974bd7918bd3a5e90
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-06-26 11:39:11 +0000

    wait

--- src/app/api/Admins/contact/route.js
+++ src/app/api/Admins/contact/route.js
@@ -1,38 +1,28 @@
-import mongoose from 'mongoose'
-// import Contacts from '@/app/Models/ContactUser';
+import prisma from '../../../libs/prismadb.jsx'
 import { NextResponse } from 'next/server';
-import { connectToDatabase } from '@/app/mongodb';
 
-async function connect() {
-    mongoose.connect(process.env.DATABASE_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(()=>console.log('databae connected............')).catch((err)=>console.log(err))
-}
-// connect();
 export async function GET() {
-    // await connect();
-    const  db  = await connectToDatabase();
-    const data = await db.collection('contactus').find().toArray()
-    console.log(data)
-    return NextResponse.json({ data: data, status:200})
+    const data = await prisma.contactus.findMany({
+    orderBy: {id: 'desc'} 
+    }) // this code will get the lastest 
+    return NextResponse.json({ data: data, status: 200 })
 
 }
-// connect();
-
 export async function POST(req) {
-    // await connect()
-    const db = await connectToDatabase();
     const { name, phone, email, message } = await req.json();
     if (!name || !phone) {
-        return NextResponse.json({data:'missing fields', status:404})
+        return NextResponse.json({ data: 'missing fields', status: 404 })
     }
-        const contact = await  db.collection('contactus').insertOne({
+    console.log(name, phone, message, email)
+    const newnumber = parseInt(phone)
+    const contact = await prisma.contactus.create({
+        data: {
             name,
-            phone,
+            phone: newnumber,
             email,
             message
-        })
-        return NextResponse.json({data:'data created', status:200, res:contact})
+        }
+    })
+    return NextResponse.json({ data: 'data created', status: 200, res: contact })
 
 }
