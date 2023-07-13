d0eacbcffa6041470d865009d7ba05730d9ec70c
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-13 20:36:04 +0000

    tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 // components
 import {
   SquarePaymentsForm,
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
         <Provider>
           <ErrorBoundary fallback={<Error/>}>
             {children}
+            <Analytics />
           </ErrorBoundary>
         </Provider>
         {/* <Footer /> */}
