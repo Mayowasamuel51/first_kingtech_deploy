339d29bbac98599821ad92a98e0851c94f448ef0
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-14 21:31:54 +0000

    contenc added dsfsf

--- src/app/cybersecurity/page.jsx
+++ src/app/cybersecurity/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Head from "next/head";
 import Experts from "../components/Experts";
 import SectionWrapper from "../components/SectionWrapper";
 import Banner from "../pagesections/cybersecurity/Banner";
@@ -10,6 +11,10 @@ import Contact from "../pagesections/home/Contact";
 function Cybersecurity() {
   return (
     <main>
+      <Head>
+        <title>cybersecurity</title>
+        <meta  content="Discover the importance of cybersecurity in the digital age and learn about effective strategies and practices to safeguard your online presence. Explore expert tips, tools, and resources to protect your personal information, defend against cyber threats, and navigate the evolving landscape of online security." title="Enhancing Online Security: Exploring the World of Cybersecurity"></meta>
+      </Head>
       <Banner />
       <div className="py-20">
         <CourseDetails />
--- src/app/security-training/page.jsx
+++ src/app/security-training/page.jsx
@@ -6,9 +6,14 @@ import Eligibility from "../pagesections/security-training.jsx/Eligibility";
 import Outline from "../pagesections/security-training.jsx/Outline";
 import Topics from "../pagesections/security-training.jsx/Topics";
 import SectionWrapper from "../components/SectionWrapper";
-function  SecurityTraining() {
+import Head from "next/head";
+function SecurityTraining() {
     return (
         <main>
+            <Head>
+                <title>security-training</title>
+                <meta content=" Prepare for the Security+ certification exam with our comprehensive guide. Learn essential cybersecurity concepts, network security, cryptography, and risk management. Gain the knowledge and skills needed to protect networks, identify vulnerabilities, and respond to security incidents. Start your journey towards a rewarding cybersecurity career today." title="Security+ Certification Guide: Boost Your Cybersecurity Career"></meta>
+            </Head>
             <Banner />
             <div className="py-20">
                 <CourseDetails />
@@ -22,4 +27,4 @@ function  SecurityTraining() {
     )
 }
 
-export default  SectionWrapper(SecurityTraining)
+export default SectionWrapper(SecurityTraining)
