cff2a841c7076b34d0e6591a5de605c12e46186c
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-17 18:02:59 +0000

    Trusted By Students Around The World

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Kingship Technologies',
-  description: 'Cyber Security e-learning platform',  
+  description: 'Trusted By Students Around The World    ,   Cyber Security e-learning platform  ',  
   keywords: "cybersecurity training, cybersecurity courses, information security, cyber defense, network security, ethical hacking, penetration testing, data privacy cyber security kingship",
   content:"Discover the importance of cybersecurity in the digital age and learn about effective strategies and practices to safeguard your online presence. Explore expert tips, tools, and resources to protect your personal information, defend against cyber threats, and navigate the evolving landscape of online security." 
 }
