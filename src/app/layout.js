717a8e8cd6ab7a7159051a17383b75d0ca84923a
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2024-01-16 08:19:06 +0100

    new commint

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
         <Provider>
           <ErrorBoundary fallback={<Error />}>
             {children}
+            
             <Analytics />
           </ErrorBoundary>
         </Provider>
