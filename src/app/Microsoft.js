d3b467254613123918c205e63151f62902c92c50
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2024-02-19 15:23:12 +0100

    neew

--- src/app/Microsoft.js
+++ src/app/Microsoft.js
@@ -6,14 +6,31 @@ const Microsoft = () => {
     <div>
       <Script strategy="afterInteractive">
         {`
-        (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "km94medx88");
+         (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3765150,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `}
       </Script>
     </div>
   );
 };
+
+// <!-- Hotjar Tracking Code for https://www.kingshiptechnologies.com/ -->
+{/* <script> */}
+    {/* (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3765150,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); */}
+// </script>
+
+
 export default Microsoft;
